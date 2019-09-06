d099350153a9fb5bb3f47f4a3d0649ab0de62ad1
snow <rhasya@gmail.com>
2019-09-06 23:02:16 +0900

    로그인 버튼 추가

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="container nav-container">
+        <a href="javascript:;" v-on:click="handleClick">Login</a>
+      </div>
     </div>
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  methods: {
+    handleClick: function() {
+      alert(1);
+    }
+  }
 }
+</script>
+<style scoped>
 #nav {
-  padding: 30px;
+  height: 24px;
+  background-color: black;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.nav-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 1024px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-container a {
+  color: white;
+}
+.nav-container a:visited {
+  color: white;
+}
+.nav-container a:hover {
+  color: yellow;
 }
 </style>
